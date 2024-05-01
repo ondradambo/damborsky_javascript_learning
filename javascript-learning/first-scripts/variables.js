dd6064c06669785fc5b39db6e2997aea8232b327
Ondrej Damborsky <dambo@Ondrej-MacBook-Air-2.local>
2024-05-01 22:21:02 +0200

    JS opakovani

--- javascript-learning/first-scripts/variables.js
+++ javascript-learning/first-scripts/variables.js
@@ -1,5 +1,5 @@
 let firstName = "Petr";
 console.log(firstName);
-firstName = "Alžběta";
+firstName = "Alžběta"; //změna proměnné firstName (tady už bez "let", to je jen pro vytvoření), spuštění vypíše nově zvolenou hodnotu proměnné
 firstName = "Bafetimbi";
 console.log(firstName);
